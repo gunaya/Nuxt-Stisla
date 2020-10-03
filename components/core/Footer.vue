b5c762ecfab6b6a7565c873b082d1d62612bc96a
Gunaya <gunnayaa@gmail.com>
2020-10-03 17:14:30 +0800

    update footer

--- components/core/Footer.vue
+++ components/core/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="main-footer">
         <div class="footer-left">
-            Copyright &copy; 2020 <div class="bullet"></div> Converted By <a href="https://github.com/gunaya">Gunaya</a>
+            Copyright &copy; 2020 <div class="bullet"></div> Converted With <i class="fas fa-heart"></i> By <a href="https://github.com/gunaya">Gunaya</a>
         </div>
         <div class="footer-right">1.0.0</div>
     </footer>
