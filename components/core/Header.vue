4a28e449450bace7e02cd212f36b1d019d278d26
Gunaya <gunnayaa@gmail.com>
2020-10-14 11:47:58 +0800

    update

--- components/core/Header.vue
+++ components/core/Header.vue
@@ -118,7 +118,7 @@
 
             <!-- Profile Dropdown -->
             <li class="dropdown">
-                <a href="" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user" >
+                <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user" >
                     <img alt="image" src="~/assets/img/avatar/avatar-1.png" class="rounded-circle mr-1" />
                     <div class="d-sm-none d-lg-inline-block">
                         Hi, Zucc
--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     script: [
       { src: 'https://code.jquery.com/jquery-3.3.1.min.js' },
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' },
-      // { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' },
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' },
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js' },
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js' },
     ],
